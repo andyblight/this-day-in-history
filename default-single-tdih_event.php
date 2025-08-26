97d8374fe5e9b858698941c9dbf21104e412f000
Andy Blight <andy.blight@gmail.com>
2025-08-26 16:44:54 +0100

    Fixed security issues in default-single-tdih_event.php

--- default-single-tdih_event.php
+++ default-single-tdih_event.php
@@ -4,15 +4,25 @@ get_header();
 
 the_post();
 
-$eid = get_the_ID();
+$eid = absint( get_the_ID() );
 
-echo '<article id="tdih-'.$eid.'" class="tdih_event">';
+echo '<article id="tdih-' . esc_attr( $eid ) . '" class="tdih_event">';
 
-the_title('<h2>'.__('This Day in History: ', 'this-day-in-history'), '</h2>');
+the_title( '<h2>' . esc_html__( 'This Day in History: ', 'this-day-in-history' ), '</h2>' );
 
 the_content();
 
-if (current_user_can('manage_tdih_events')) { echo '<footer><a href="'.admin_url("admin.php?page=this-day-in-history&action=edit&id=".$eid).'">'.__('Edit Event', 'this-day-in-history').'</a></footer>'; }
+if ( current_user_can( 'manage_tdih_events' ) ) {
+    $edit_url = add_query_arg(
+        array(
+            'page'   => 'this-day-in-history',
+            'action' => 'edit',
+            'id'     => $eid,
+        ),
+        admin_url( 'admin.php' )
+    );
+    echo '<footer><a href="' . esc_url( $edit_url ) . '">' . esc_html__( 'Edit Event', 'this-day-in-history' ) . '</a></footer>';
+}
 
 echo '</article>';
 
